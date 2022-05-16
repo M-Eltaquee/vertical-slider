b750f4dc87af8201b82809d5618ab8796260cc93
taquee <mohamed.eltaquee1987@gmail.com>
2022-05-16 11:31:01 +0400

    change pics and logic

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ const slideLength = document.querySelectorAll('.right-item').length
 
 let slideIndex = 1
 
-leftSide.style.top = `-${(slideLength-slideIndex)* 100}vh`
-rightSide.style.top = `${(slideLength-slideIndex -3) * 100}vh`
+// leftSide.style.top = `-${(slideLength - slideIndex) * 100}vh`
+// rightSide.style.top = `${(slideLength - slideIndex - 3) * 100}vh`
 upBotton.addEventListener('click', () => changeSlide())
 downBotton.addEventListener('click', () => changeSlide())
 
 const changeSlide = () => {
     slideIndex++
-    if(slideIndex > slideLength){
+    if (slideIndex > slideLength) {
         slideIndex = 1
     }
-    leftSide.style.top = `-${(slideLength-slideIndex)* 100}vh`
-    rightSide.style.top = `${(slideLength-slideIndex -3) * 100}vh`
+    leftSide.style.transform = `translateY(-${(slideLength - slideIndex) * 100}vh)`
+    rightSide.style.transform = `translateY(-${(slideIndex - 1) * 100}vh)`
 }
